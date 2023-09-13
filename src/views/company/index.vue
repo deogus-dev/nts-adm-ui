bdca3b1316bc401c2ff4544a252f76d3d16f87c6
Deahyeon Jang <zdh8456@gmail.com>
2023-09-13 17:14:52 +0900

    first commit

---
+++ src/views/company/index.vue
@@ -0,0 +1,62 @@
+<script setup>
+import company from './company.vue'
+import team from './team.vue'
+</script>
+
+<template>
+  <div class="row m-0 h-100">
+    <div class="col-6 h-100 overflow-auto"><company /></div>
+    <div class="col-6">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="home-tab-pane"
+            aria-selected="true"
+          >
+            팀
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="profile-tab-pane"
+            aria-selected="false"
+          >
+            위치
+          </button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div
+          class="tab-pane fade show active"
+          id="home-tab-pane"
+          role="tabpanel"
+          aria-labelledby="home-tab"
+          tabindex="0"
+        >
+          <team />
+        </div>
+        <div
+          class="tab-pane fade"
+          id="profile-tab-pane"
+          role="tabpanel"
+          aria-labelledby="profile-tab"
+          tabindex="0"
+        >
+          ...
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/company/team.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <span>팀등록</span>
+</template>
